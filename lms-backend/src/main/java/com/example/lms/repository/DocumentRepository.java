3966771e2c0a0a8e481338ec14b3760e5527ba64
tuan0602 <anhtuan060201@gmail.com>
2025-11-12 11:10:33 +0700

    Authentication backend

--- lms-backend/src/main/java/com/example/lms/repository/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.lms.repository;
-
-import com.example.lms.entity.Document;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface DocumentRepository extends JpaRepository<Document, String> {
-    List<Document> findByCourseId(String courseId);
-}
--- lms-backend/src/main/java/com/example/lms/repository/ReplyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.lms.repository;
-
-import com.example.lms.entity.Reply;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ReplyRepository extends JpaRepository<Reply, String> {
-    List<Reply> findByDiscussionId(String discussionId);
-}
