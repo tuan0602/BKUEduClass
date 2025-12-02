d7c194c59247fae07571214a28cc2e1418c27da5
tuancapoo <tuan.nguyencapoo@hcmut.edu.vn>
2025-12-03 04:48:19 +0700

    configauth for frontend

--- lms-backend/src/main/java/com/example/demo/config/UserDetailsCustom.java
+++ lms-backend/src/main/java/com/example/demo/config/UserDetailsCustom.java
@@ -1,34 +1,34 @@
-// package com.example.demo.config;
+package com.example.demo.config;
 
-// import com.example.demo.service.UserService;
-// import org.springframework.security.core.authority.SimpleGrantedAuthority;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.stereotype.Component;
+import com.example.demo.service.AuthService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
-// import java.util.Collections;
+import java.util.Collections;
 
-// @Component("userDetailsCustom")
-// public class UserDetailsCustom implements UserDetailsService {
-//     private final UserService userService;
-//     public UserDetailsCustom(UserService userService) {
-//         this.userService = userService;
-//     }
+@Component("userDetailsCustom")
+public class UserDetailsCustom implements UserDetailsService {
+    private final AuthService authService;
+    public UserDetailsCustom(AuthService authService) {
+        this.authService = authService;
+    }
 
 
-//     @Override
-//     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//         com.example.demo.domain.User user= userService.getUserByEmail(username);
-//         if (user==null)
-//             throw new UsernameNotFoundException(username);
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.example.demo.domain.User user= authService.getUserByEmail(username);
+        if (user==null)
+            throw new UsernameNotFoundException(username);
 
-//         User us= new User(
-//                 user.getEmail(),
-//                 user.getPassword(),
-//                 Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+user.getRole().name()))
-//         );
-//         return us;
-//     }
-// }
+        User us= new User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+user.getRole().name()))
+        );
+        return us;
+    }
+}
