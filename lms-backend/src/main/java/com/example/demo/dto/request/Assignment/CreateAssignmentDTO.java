4c2a43ac5ef8db50559b6e4c98a3baa83f3cf952
tuancapoo <tuan.nguyencapoo@hcmut.edu.vn>
2025-12-04 14:43:30 +0700

    Sua Assignment cho giong front end

--- lms-backend/src/main/java/com/example/demo/dto/request/Assignment/CreateAssignmentDTO.java
+++ lms-backend/src/main/java/com/example/demo/dto/request/Assignment/CreateAssignmentDTO.java
@@ -1,19 +1,34 @@
 package com.example.demo.dto.request.Assignment;
 
+import com.example.demo.domain.enumeration.Answer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateAssignmentDTO {
-    private Long courseId;
-    private String title;
-    private String description;
-    private LocalDateTime dueDate;
+    public Long courseId;
+    public String title;
+    public String description;
+    public LocalDateTime dueDate;
+    public List<QuestionDTO> question;
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class QuestionDTO{
+        private String question;
+        private String answerA;
+        private String answerB;
+        private String answerC;
+        private String answerD;
+        private Answer correctAnswer;
+    }
 }
